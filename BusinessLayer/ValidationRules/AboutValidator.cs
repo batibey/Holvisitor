52b6f9caf51fe865dfca974de0e40c0bad9b477c
mustafabati <mustafa.bati9@gmail.com>
2023-03-22 22:09:23 +0300

    ValidationRules

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class AboutValidator : AbstractValidator<About>
+	{
+		public AboutValidator()
+		{
+			RuleFor(x => x.Description).NotEmpty().WithMessage("Description Required");
+			RuleFor(x => x.Image1).NotEmpty().WithMessage("Please Image Select");
+			RuleFor(x => x.Description).MinimumLength(20).WithMessage("Minimum 20 Character");
+			RuleFor(x => x.Description).MaximumLength(1000).WithMessage("Maximum 1000 Character");
+		}
+	}
+}
+
---
+++ hol.visitor/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hol.visitor.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
