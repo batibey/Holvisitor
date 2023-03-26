ad28e08e5d4c13c2755e19ec396d7381c537fd79
mustafabati <mustafa.bati9@gmail.com>
2023-03-26 19:02:17 +0300

    liberty

---
+++ hol.visitor/Controllers/DestinationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace hol.visitor.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDAL());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
+
