0d506ece596a3bf5b31ccba787292c0fc11c94e3
mustafabati <mustafa.bati9@gmail.com>
2023-03-29 22:20:29 +0300

    Comments

---
+++ hol.visitor/ViewComponents/Comment/_CommentList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hol.visitor.ViewComponents.Comment
+{
+	public class _CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
