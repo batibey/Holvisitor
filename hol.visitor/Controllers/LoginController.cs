70dfe4a389348f2d257ad6822a6ec1d9ccbc237e
mustafabati <mustafa.bati9@gmail.com>
2023-04-04 19:04:12 +0300

    login and register

--- hol.visitor/Controllers/LoginController.cs
+++ hol.visitor/Controllers/LoginController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hol.visitor.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
-        
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //  return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
         {
             return View();
         }
