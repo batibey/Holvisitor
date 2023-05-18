e7e9c3cf81470f801c5c1e41ec2c2eb358593c36
mustafabati <mustafa.bati9@gmail.com>
2023-05-18 22:56:43 +0300

    development

---
+++ hol.visitor/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace hol.visitor.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    [Area("Member")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDAL());
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
+
