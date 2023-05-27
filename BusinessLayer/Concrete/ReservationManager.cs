b247869c75d3ef0c9325a2e8e8017fb5e9633269
mustafabati <mustafa.bati9@gmail.com>
2023-05-27 20:10:59 +0300

    reservation booking

---
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDAL _reservationDAL;
+
+        public ReservationManager(IReservationDAL reservationDAL)
+        {
+            _reservationDAL = reservationDAL;
+        }
+
+        public void TAdd(Reservation t)
+        {
+            _reservationDAL.Insert(t);
+        }
+
+        public void TDelete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
--- hol.visitor/Areas/Member/Controllers/ReservationController.cs
+++ hol.visitor/Areas/Member/Controllers/ReservationController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace hol.visitor.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDAL());
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDAL());
+
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -24,13 +29,22 @@ namespace hol.visitor.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 1;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
     }
 }
