8c575df676c444b61d9a5b09b8271e9c4512491d
mustafabati <mustafa.bati9@gmail.com>
2023-05-31 22:21:46 +0300

    guides profileInformation and PlatformSetting

---
+++ BusinessLayer/Concrete/GuideManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class GuideManager : IGuideService
+    {
+        IGuideDAL _guideDAL;
+
+        public GuideManager(IGuideDAL guideDAL)
+        {
+            _guideDAL = guideDAL;
+        }
+
+        public void TAdd(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guide TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guide> TGetList()
+        {
+            return _guideDAL.GetList();
+        }
+
+        public void TUpdate(Guide t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
