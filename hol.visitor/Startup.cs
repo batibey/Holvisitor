ce4c9b72defd0d45ae3f5e3fc0e9e819ee356d52
mustafabati <mustafa.bati9@gmail.com>
2023-06-10 15:39:17 +0300

    development

--- hol.visitor/Startup.cs
+++ hol.visitor/Startup.cs
@@ -52,7 +52,6 @@ namespace hol.visitor
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             var path = Directory.GetCurrentDirectory();
