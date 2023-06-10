86a04f80b772c9aea7e1a9d2437ba0f3bfd54e70
mustafabati <mustafa.bati9@gmail.com>
2023-06-10 15:16:55 +0300

    development

--- hol.visitor/Startup.cs
+++ hol.visitor/Startup.cs
@@ -24,7 +24,6 @@ namespace hol.visitor
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -66,7 +65,6 @@ namespace hol.visitor
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
