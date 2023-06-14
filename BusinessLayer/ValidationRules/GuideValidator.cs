e80ac6972ca2b6346890c0a361c40b5b9aab50cd
mustafabati <mustafa.bati9@gmail.com>
2023-06-14 22:18:16 +0300

    guide validator edit

--- BusinessLayer/ValidationRules/GuideValidator.cs
+++ BusinessLayer/ValidationRules/GuideValidator.cs
@@ -12,11 +12,10 @@ namespace BusinessLayer.ValidationRules
     {
         public GuideValidator()
         {
-            RuleFor(x => x.Name).NotEmpty().WithMessage("Lütfen rehber adını giriniz");
-            RuleFor(x => x.Description).NotEmpty().WithMessage("Lütfen rehber açıklamasını giriniz");
-            RuleFor(x => x.Image).NotEmpty().WithMessage("Lütfen rehber görselini giriniz");
-            RuleFor(x => x.Name).MaximumLength(30).WithMessage("lütfen 30 karakterden daha kısa bir isim giriniz");
-            RuleFor(x => x.Name).MinimumLength(8).WithMessage("lütfen 8 karakterden daha uzun bir isim giriniz");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Please Guide Name Entry");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please Guide Description Entry");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Please Guide Image Entry");
+            
         }
     }
 }
