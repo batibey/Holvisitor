eb46250ff403b701e1b5e48092a847e793dd2c1d
mustafabati <mustafa.bati9@gmail.com>
2023-07-02 16:13:43 +0300

    send mail

---
+++ hol.visitor/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,46 @@
+﻿
+using hol.visitor.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace hol.visitor.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            MimeMessage mimeMessage = new();
+
+            MailboxAddress mailboxAddressFrom = new("Admin", "mustafa.bati9@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new("User", mailRequest.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder
+            {
+                TextBody = mailRequest.Body
+            };
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailRequest.Subject;
+
+            SmtpClient client = new();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("mustafa.bati9@gmail.com", "jtjodunuutmqonuq");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
+
---
+++ hol.visitor/Models/MailRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace hol.visitor.Models
+{
+	public class MailRequest
+	{
+        public string? Name { get; set; }
+
+        public string? SenderMail { get; set; }
+
+		public string? ReceiverMail { get; set; }
+
+        public string? Subject { get; set; }
+
+        public string? Body { get; set; }
+    }
+}
+
