0f4e7b4820eb9f311796d6e7af252c40570062ac
mustafabati <mustafa.bati9@gmail.com>
2023-08-01 20:46:40 +0300

    development

--- hol.visitor/Areas/Admin/Controllers/BookingHotelSearchController.cs
+++ hol.visitor/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using hol.visitor.Areas.Admin.Models;
+using System.Net.Http.Headers;
 
 namespace hol.visitor.Areas.Admin.Controllers
 {
@@ -20,24 +16,22 @@ namespace hol.visitor.Areas.Admin.Controllers
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?room_number=1&dest_type=city&order_by=popularity&dest_id=-1456928&locale=en-gb&checkin_date=2023-01-27&filter_by_currency=EUR&checkout_date=2023-01-28&adults_number=2&units=metric&children_ages=5%2C0&include_adjacency=true&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&children_number=2"),
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v2/hotels/search?order_by=popularity&adults_number=2&checkin_date=2023-09-27&filter_by_currency=AED&dest_id=-553173&locale=en-gb&checkout_date=2023-09-28&units=metric&room_number=1&dest_type=city&include_adjacency=true&children_number=2&page_number=0&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1"),
                 Headers =
     {
         { "X-RapidAPI-Key", "9c12b0dd65mshe7c68d5111f5868p145776jsn966eb301c2d4" },
         { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
     },
             };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                var bodyReplace = body.Replace(".", "");
-                var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(bodyReplace);
-                return View(values.result);
-            }
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            var bodyReplace = body.Replace(".", "");
+            var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(bodyReplace);
+            return View(values?.result);
         }
 
-        [HttpGet]
+[HttpGet]
         public IActionResult GetCityDestID()
         {
             return View();
