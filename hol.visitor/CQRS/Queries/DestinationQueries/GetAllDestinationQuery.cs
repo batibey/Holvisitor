197cba9325448a13794b91b1e3ee700ffcbd33e1
mustafabati <mustafa.bati9@gmail.com>
2023-08-06 21:48:07 +0300

    cqrs pattern completed

---
+++ hol.visitor/CQRS/Queries/DestinationQueries/GetAllDestinationQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hol.visitor.CQRS.Queries.DestinationQueries
+{
+    public class GetAllDestinationQuery
+    {
+    }
+}
---
+++ hol.visitor/CQRS/Queries/GuideQueries/GetAllGuideQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hol.visitor.CQRS.Results.GuideResults;
+
+namespace hol.visitor.CQRS.Queries.GuideQueries
+{
+    public class GetAllGuideQuery:IRequest<List<GetAllGuideQueryResult>>
+    {
+    }
+}
