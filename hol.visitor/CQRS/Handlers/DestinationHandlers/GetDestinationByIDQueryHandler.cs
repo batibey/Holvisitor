ac32dc489683ff92ff9c3a15a222216df89f713e
mustafabati <mustafa.bati9@gmail.com>
2023-08-16 21:49:24 +0300

    development

--- hol.visitor/CQRS/Handlers/DestinationHandlers/GetDestinationByIDQueryHandler.cs
+++ hol.visitor/CQRS/Handlers/DestinationHandlers/GetDestinationByIDQueryHandler.cs
@@ -1,8 +1,4 @@
 ﻿using DataAccessLayer.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using hol.visitor.CQRS.Queries.DestinationQueries;
 using hol.visitor.CQRS.Results.DestinationResults;
 
