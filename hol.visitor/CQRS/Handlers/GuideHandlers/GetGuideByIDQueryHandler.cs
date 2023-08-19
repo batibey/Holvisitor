d1fb7e26344ea886199a532cc2c84fc6921c35e2
mustafabati <mustafa.bati9@gmail.com>
2023-08-19 12:43:16 +0300

    development

--- hol.visitor/CQRS/Handlers/GuideHandlers/GetGuideByIDQueryHandler.cs
+++ hol.visitor/CQRS/Handlers/GuideHandlers/GetGuideByIDQueryHandler.cs
@@ -1,10 +1,5 @@
 ﻿using DataAccessLayer.Concrete;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using hol.visitor.CQRS.Queries.GuideQueries;
 using hol.visitor.CQRS.Results.GuideResults;
 
